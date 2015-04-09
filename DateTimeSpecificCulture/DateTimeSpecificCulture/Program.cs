82f430e86d07e9d8a392dc3a0c7ac62188c860f3
mookrs <mookrs@gmail.com>
2015-04-09 23:33:27 +0800

    DateTime formatting and SpecificCulture
    see: https://msdn.microsoft.com/en-us/library/8kb3ddd4

---
+++ DateTimeSpecificCulture/DateTimeSpecificCulture/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeSpecificCulture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime date = DateTime.Now;
+            // See: https://msdn.microsoft.com/en-us/library/8kb3ddd4 
+            Console.WriteLine(date.ToString("yyyy-mm-dd tt hh:mm:ss", CultureInfo.CreateSpecificCulture("zh-CN")));
+            Console.ReadKey();
+        }
+    }
+}
