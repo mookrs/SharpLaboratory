83ffdda30469586dea2c668e8a27ad5aebd0579b
mookrs <mookrs@gmail.com>
2015-04-17 23:47:40 +0800

    Add a layout sample

---
+++ LayoutBasic/LayoutBasic/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace LayoutBasic
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MainWindow_OnMouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Title = "Source = " + e.Source.GetType().Name + ", OriginalSource = " + e.OriginalSource.GetType().Name +
+                         " @ " + e.Timestamp;
+
+            var source = e.Source as Control;
+
+            if (source != null && source.BorderThickness != new Thickness(5))
+            {
+                source.BorderThickness = new Thickness(5);
+                source.BorderBrush = Brushes.Black;
+            }
+            else
+            {
+                if (source != null) source.BorderThickness = new Thickness(0);
+            }
+        }
+    }
+}
