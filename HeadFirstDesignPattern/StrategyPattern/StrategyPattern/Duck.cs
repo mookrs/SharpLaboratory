458b62e1493713f7851180cb71156e7f7d8ebf37
mookrs <mookrs@gmail.com>
2015-04-23 23:57:01 +0800

    Basic Duck

---
+++ HeadFirstDesignPattern/StrategyPattern/StrategyPattern/Duck.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrategyPattern
+{
+    public abstract class Duck
+    {
+        public void Quack()
+        {
+            
+        }
+
+        public void Swim()
+        {
+            
+        }
+
+        public abstract void Display();
+    }
+}
---
+++ HeadFirstDesignPattern/StrategyPattern/StrategyPattern/MallardDuck.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrategyPattern
+{
+    public class MallardDuck : Duck
+    {
+        public override void Display()
+        {
+            Console.WriteLine("I have red head.");
+        }
+    }
+}
