3a4a3258b162daf559292f901d82a4c27996a74c
mookrs <mookrs@gmail.com>
2015-05-04 00:12:12 +0800

    Add HeatIndexDisplay

--- HeadFirstDesignPattern/ObserverPattern/ObserverPattern/Program.cs
+++ HeadFirstDesignPattern/ObserverPattern/ObserverPattern/Program.cs
@@ -11,6 +11,7 @@ namespace ObserverPattern
             CurrentConditionsDisplay currentDisplay = new CurrentConditionsDisplay(weatherData);
             StatisticsDisplay statisticsDisplay = new StatisticsDisplay(weatherData);
             ForecastDisplay forecastDisplay = new ForecastDisplay(weatherData);
+            HeatIndexDisplay heatIndexDisplay = new HeatIndexDisplay(weatherData);
 
             weatherData.SetMeasurements(80, 65, 30.4f);
             weatherData.SetMeasurements(82, 70, 29.2f);
