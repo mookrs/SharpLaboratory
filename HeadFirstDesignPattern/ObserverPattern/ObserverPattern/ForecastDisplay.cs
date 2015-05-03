7f1786ebe0849be9b3fd2c647a1a1c2b9b245f35
mookrs <mookrs@gmail.com>
2015-05-03 23:59:17 +0800

    Finish three podcast

--- HeadFirstDesignPattern/ObserverPattern/ObserverPattern/ForecastDisplay.cs
+++ HeadFirstDesignPattern/ObserverPattern/ObserverPattern/ForecastDisplay.cs
@@ -1,20 +1,45 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ObserverPattern
 {
     /// <summary>
     /// 天气预报
     /// </summary>
-    public class ForecastDisplay
+    public class ForecastDisplay : IObserver, IDisplayElement
     {
+        private float _currentPressure = 29.92f;
+        private float _lastPressure;
+        private WeatherData _weatherData;
+
+        public ForecastDisplay(WeatherData weatherData)
+        {
+            _weatherData = weatherData;
+            weatherData.RegisterObserver(this);
+        }
+
         public void Update(float temp, float humidity, float pressure)
         {
-            
+            _lastPressure = _currentPressure;
+            _currentPressure = pressure;
+
+            Display();
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("Forecast: ");
+            if (_currentPressure > _lastPressure)
+            {
+                Console.WriteLine("Improving weather on the way!");
+            }
+            else if (_currentPressure == _lastPressure)
+            {
+                Console.WriteLine("More of the same");
+            }
+            else if (_currentPressure < _lastPressure)
+            {
+                Console.WriteLine("Watch out for cooler, rainy weather");
+            }
         }
     }
 }
--- HeadFirstDesignPattern/ObserverPattern/ObserverPattern/Program.cs
+++ HeadFirstDesignPattern/ObserverPattern/ObserverPattern/Program.cs
@@ -1,20 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ObserverPattern
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             WeatherData weatherData = new WeatherData();
 
             CurrentConditionsDisplay currentDisplay = new CurrentConditionsDisplay(weatherData);
-            //StatisticsDisplay statisticsDisplay = new StatisticsDisplay(weatherData);
-            //ForecastDisplay forecastDisplay = new ForecastDisplay(weatherData);
+            StatisticsDisplay statisticsDisplay = new StatisticsDisplay(weatherData);
+            ForecastDisplay forecastDisplay = new ForecastDisplay(weatherData);
 
             weatherData.SetMeasurements(80, 65, 30.4f);
             weatherData.SetMeasurements(82, 70, 29.2f);
